13e68d9a86083a59725286e6901fe8da2cf19c96
Harshad Kavathiya <harshad3011@gmail.com>
2017-09-20 19:52:05 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pybdd',
+      version='0.0.1',
+      description='This is test framework for BDD',
+      url='https://github.com/HarshadKavathiya/pybdd',
+      download_url='https://github.com/HarshadKavathiya/pybdd/archive/master.zip',
+      author='Harshad Kavathiya',
+      author_email='harshad3011@gmail.com',
+      license='MIT',
+      packages=['pybdd'],
+      zip_safe=False)
